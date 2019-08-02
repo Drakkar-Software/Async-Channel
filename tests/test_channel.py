2adf60ed056627d496c5c108ec0244a115cf7ae5
Herklos <herklos.dev@protonmail.com>
2019-08-03 00:40:44 +0200

    Fix tests

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,6 +1,22 @@
-from octobot_channels.channels import RECENT_TRADES_CHANNEL
-from octobot_channels.channels.exchange.exchange_channel import ExchangeChannels
+import pytest
+
+from octobot_channels.channels.channel import Channels, Channel
+
+TEST_CHANNEL = "Test"
 
 
-def test_get_chan():
-    ExchangeChannels.get_chan(RECENT_TRADES_CHANNEL, "")
+class TestChannel(Channel):
+    pass
+
+
+async def _create_channels():
+    for channel_class in Channel.__subclasses__():
+        channel = channel_class()
+        Channels.set_chan(channel, name=channel_class.get_name())
+        await channel.start()
+
+
+@pytest.mark.asyncio
+async def test_get_chan():
+    await _create_channels()
+    Channels.get_chan(TEST_CHANNEL)
