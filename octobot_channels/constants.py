62f64abaa6fb5ad1f7abc8c4029d4ee1804da4f3
herklos <herklos.dev@protonmail.com>
2019-11-07 23:26:07 +0100

    [Version] v1.3.15

--- octobot_channels/constants.py
+++ octobot_channels/constants.py
@@ -15,6 +15,6 @@
 #  License along with this library.
 
 PROJECT_NAME = "OctoBot-Channels"
-VERSION = "1.3.14"  # major.minor.revision
+VERSION = "1.3.15"  # major.minor.revision
 
 CHANNEL_WILDCARD = "*"
