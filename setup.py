119e13afd6b2e32197cad8ee74273c680dbba485
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2023-05-02 17:27:02 +0200

    [Cython] remove cython

--- setup.py
+++ setup.py
@@ -14,48 +14,16 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 # from distutils.extension import Extension
-import os
-
-from setuptools import dist
-dist.Distribution().fetch_build_eggs(['Cython>=0.29.32'])
-
-try:
-    from Cython.Distutils import build_ext
-    from Cython.Build import cythonize
-except ImportError:
-    # create closure for deferred import
-    def cythonize(*args, **kwargs):
-        from Cython.Build import cythonize
-        return cythonize(*args, **kwargs)
-
-    def build_ext(*args, **kwargs):
-        from Cython.Distutils import build_ext
-        return build_ext(*args, **kwargs)
-
 from setuptools import find_packages
-from setuptools import setup, Extension
+from setuptools import setup
 
 from async_channel import PROJECT_NAME, VERSION
 
 PACKAGES = find_packages(exclude=["tests"])
 
-packages_list = ["async_channel.consumer",
-                 "async_channel.producer",
-                 "async_channel.channels.channel",
-                 "async_channel.channels.channel_instances",
-                 "async_channel.util.channel_creator"]
-
-ext_modules = [
-    Extension(package, [f"{package.replace('.', '/')}.py"])
-    for package in packages_list]
-
-# long description from README file
-# with open('README.md', encoding='utf-8') as f:
-#     DESCRIPTION = f.read()
 
 REQUIRED = open('requirements.txt').readlines()
 REQUIRES_PYTHON = '>=3.8'
-CYTHON_DEBUG = False if not os.getenv('CYTHON_DEBUG') else os.getenv('CYTHON_DEBUG')
 
 setup(
     name=PROJECT_NAME,
@@ -63,19 +31,16 @@ setup(
     url='https://github.com/Drakkar-Software/Async-Channel',
     license='LGPL-3.0',
     author='Drakkar-Software',
-    author_email='drakkar-software@protonmail.com',
+    author_email='contact@drakkar.software',
     description='Python channel based communication library',
     packages=PACKAGES,
     include_package_data=True,
-    # long_description=DESCRIPTION,
-    cmdclass={'build_ext': build_ext},
     tests_require=["pytest"],
     test_suite="tests",
     zip_safe=False,
     data_files=[],
-    setup_requires=REQUIRED if not CYTHON_DEBUG else [],
+    setup_requires=REQUIRED,
     install_requires=REQUIRED,
-    ext_modules=cythonize(ext_modules, gdb_debug=CYTHON_DEBUG),
     python_requires=REQUIRES_PYTHON,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
